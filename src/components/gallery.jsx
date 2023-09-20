3f33e6199ef7b3294d897941c6edb3eccf8060aa
Victorvalour <ewehvictor7@gmail.com>
2023-09-20 15:48:20 +0100

    update

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import car1 from  "../images/car1.jpg"
 import car2 from  "../images/car2.jpg"
 import car3 from  "../images/car3.jpg"
@@ -19,39 +19,39 @@ const cars = [
         src: car1,
     
     },
-    {   name: "car1",
+    {   name: "car2",
         id: "car2",
         src: car2
     },
-    {   name: "car1",
+    {   name: "car3",
         id: "car3",
         src: car3
     },
-    {   name: "car1",
+    {   name: "car4",
         id: "car4",
         src: car4
     },
-    {   name: "car1",
+    {   name: "car5",
         id: "car5",
         src: car5
     },
-    {   name: "car1",
+    {   name: "car6",
         id: "car6",
         src: car6
     },
-    {   name: "car1",
+    {   name: "car7",
         id: "car7",
         src: car7
     },
-    {   name: "car1",
+    {   name: "car8",
         id: "car8",
         src: car8
     },
-    {   name: "car1",
+    {   name: "car9",
         id: "car9",
         src: car9
     },
-    {   name: "car1",
+    {   name: "car10",
         id: "car10",
         src: car10
     }
@@ -59,14 +59,37 @@ const cars = [
 ]
 
 export default function Gallery() {
+    const [mycars, setMycars] = useState(cars);
+
+const handleDragDrop = (results) => {
+   const {source, destination, type} = results;
+   console.log(results)
+
+    if (!destination) return;
+    if(source.droppableId === destination.droppableId && source.index === destination.index) return;
+    if (type === 'group') {
+        const reorderedCars = [...mycars];
+        
+        const sourceIndex = source.index;
+        const destinationIndex = destination.index;
+
+        const [removedCar] = reorderedCars.splice(sourceIndex, 1);
+        reorderedCars.splice(destinationIndex, 0, removedCar);
+
+        return setMycars(reorderedCars);
+    }
+
+  }
+
     return (<div>
-        <DragDropContext onDragEnd={() => {console.log("drag and drop has occured")}}>
-           
-        <div>
-        <Droppable droppableId="ROOT" type="group">
-           {(provided) => (
-            <div {...provided.droppableProps} ref={provided.innerRef} className="flex gap-5 flex-wrap">
-                 {cars.map((car, index) => (
+        <DragDropContext onDragEnd={handleDragDrop}>
+        <Droppable droppableId="ROOT" type="group">  
+        {(provided) => (
+        <div {...provided.droppableProps} ref={provided.innerRef}  className="bg-red-600">
+       
+          
+            <div className="flex gap-5 flex-wrap">
+                 {mycars.map((car, index) => (
                     <Draggable draggableId={car.id} key={car.id} index={index}>
                         {(provided) => (
                              <div className="border-2 border-sky-500" {...provided.dragHandleProps} {...provided.draggableProps} ref={provided.innerRef}><img src={car.src} alt="" className="w-60 h-60"/>
@@ -75,11 +98,12 @@ export default function Gallery() {
                
                 </Draggable>
             ))}
+                     {provided.placeholder}
             </div>
-           )}
-           </Droppable>
-        </div>
-        
+     
+      
+        </div> )}
+        </Droppable>
         </DragDropContext>
         </div>
     )
